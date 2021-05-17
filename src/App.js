e268634a8dfa08d3760cac8beab305af4283f56f
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-05-17 13:44:06 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/leila-mahmoudi-96a417202/"
+          target="-blank"
+        >
+          leila mahmoudi
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/leilamahmoudi/weather-react-finally"
+          target="-blank"
+        >
+          {" "}
+          GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
