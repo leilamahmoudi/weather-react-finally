8a91ef072eed546db6610e2ae0de0c189f6dfd87
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-05-17 14:41:49 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/leila-mahmoudi-96a417202/"
-          target="-blank"
-        >
-          leila mahmoudi
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/leilamahmoudi/weather-react-finally"
-          target="-blank"
-        >
-          {" "}
-          GitHub
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/leila-mahmoudi-96a417202/"
+            target="-blank"
+          >
+            leila mahmoudi
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/leilamahmoudi/weather-react-finally"
+            target="-blank"
+          >
+            {" "}
+            GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
