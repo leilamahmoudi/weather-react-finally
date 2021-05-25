6e0247b4e0b3ae4e162cb446962cad2e73e3eab0
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-05-25 14:02:12 +0200

    Made icon dynamic

--- src/Weather.js
+++ src/Weather.js
@@ -18,8 +18,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
-      iconUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0tsrQFHHBaToPBlVTW8Wq6lnlxpwPt9txeg&usqp=CAU",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
   function getData() {
